99dbe98db1c8105d5daa7d63cfe223638687ea14
farfromrefuge <dev@akylas.fr>
2023-01-20 15:51:07 +0100

    first commit

---
+++ app/app.ts
@@ -0,0 +1,26 @@
+/*
+In NativeScript, the app.ts file is the entry point to your application.
+You can use this file to perform app-level initialization, but the primary
+purpose of the file is to pass control to the app’s first module.
+*/
+
+import { ContentView, aliasTagName, globalRegister, makeListView, registerDOMElement, registerElement } from 'dominative';
+import App from './App.svelte';
+import { CollectionView } from '@nativescript-community/ui-collectionview';
+import { Event } from 'undom-ng';
+import { Application } from '@nativescript/core';
+
+Event.prototype.initCustomEvent = Event.prototype.initEvent;
+globalRegister(global);
+registerElement('CollectionView', CollectionView);
+// registerElement('ContentView', ContentView);
+registerDOMElement('dummy');
+
+aliasTagName((tag) => tag.toLowerCase());
+
+//@ts-ignore
+
+//@ts-ignore
+const app = new App({ target: document });
+//@ts-ignore
+Application.run({ create: () => app.$$.root });
---
+++ svelte.config.js
@@ -0,0 +1,16 @@
+const sveltePreprocess = require('svelte-preprocess');
+
+module.exports = {
+    compilerOptions: {
+        namespace: 'foreign'
+    },
+    preprocess: [
+        sveltePreprocess({
+            typescript: {
+                compilerOptions: {
+                    target: 'es2020'
+                }
+            }
+        })
+    ]
+};
