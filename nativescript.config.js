4991b4da126adb4b402ea27a141148f26a21a629
farfromrefuge <dev@akylas.fr>
2023-01-20 17:31:30 +0100

    chore: for timeline tests

---
+++ nativescript.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    id: 'org.nativescript.dominativedemosvelte',
+    appPath: 'app',
+    appResourcesPath: 'App_Resources',
+    android: {
+        markingMode: 'none',
+        codeCache: true
+    },
+    cssParser: 'rework'
+};
