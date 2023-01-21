af519b774920502f2713e580163c656be9932411
Yukino Song <yukino@yuki.network>
2023-01-22 04:42:39 +0800

    distinguish two branches

--- nativescript.config.js
+++ nativescript.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    id: 'org.nativescript.dominativedemosvelte',
+    id: 'org.nativescript.dominativedemosvelte_1',
     appPath: 'app',
     webpackConfigPath: 'app.webpack.config.js',
     appResourcesPath: 'App_Resources',
