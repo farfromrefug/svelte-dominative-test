8a37e7baa45a574fac1c4f897a9e56a364299dd7
farfromrefuge <dev@akylas.fr>
2023-01-23 09:51:53 +0100

    chore: a lot more complex layout to test perfs

--- app/CustomComponent.svelte
+++ app/CustomComponent.svelte
@@ -1,29 +1,41 @@
 <script lang="ts">
-export let item
-export let index
+    export let item;
+    export let index;
 </script>
 
 <gridlayout rows="*, auto, auto" backgroundColor={item.color} class="item">
-	<htmllabel text={`Odd ${item.index}`}/>
-	<htmllabel text={`Odd ${item.index}`}/>
-	<htmllabel text={`Odd ${item.index}`}/>
-	<htmllabel text={`Odd ${item.index}`}/>
-	<htmllabel text={`Odd ${item.index}`}/>
-	<htmllabel text={`Odd ${item.index}`}/>
-	<htmllabel text={`Odd ${item.index}`}/>
-	<htmllabel text={`Odd ${item.index}`}/>
-	<htmllabel text={`Odd ${item.index}`}/>
-	<stacklayout row="1">
-		<htmllabel text={`Odd ${index}`}/>
-		<htmllabel row="1" text={item.name} class="title" />
-		<htmllabel row="1" text={item.color} class="subtitle" />
-	</stacklayout>
-	<button text="button" />
-	<button text="button" />
-	<button text="button" />
-	<button text="button" />
-	<button text="button" />
-	<button text="button" />
+    <htmllabel text={`Odd ${item.index}`} />
+    <htmllabel html={`Odd ${item.index}`} />
+    <htmllabel html={`Odd ${item.index}`} />
+    <htmllabel html={`Odd ${item.index}`} />
+    <htmllabel html={`Odd ${item.index}`} />
+    <htmllabel html={`Odd ${item.index}`} />
+    <htmllabel html={`Odd ${item.index}`} />
+    <htmllabel html={`Odd ${item.index}`} />
+    <htmllabel html={`Odd ${item.index}`} />
+    <stacklayout row="1">
+        <htmllabel text={`Odd ${index}`} />
+        <htmllabel row="1" html={`<span style="color:${item.color}">${item.name}</span>`} class="title" backgroundColor="black" />
+        <htmllabel row="1" html={`<span style="color:${item.color}">${item.color}</span>`} class="subtitle" backgroundColor="black" />
+    </stacklayout>
+    <button text="button">
+        <span text={`Button ${item.index}`} />
+    </button>
+    <button text="button">
+        <span text={`Button ${item.index}`} />
+    </button>
+    <button text="button">
+        <span text={`Button ${item.index}`} />
+    </button>
+    <button text="button">
+        <span text={`Button ${item.index}`} />
+    </button>
+    <button text="button">
+        <span text={`Button ${item.index}`} />
+    </button>
+    <button text="button">
+        <span text={`Button ${item.index}`} />
+    </button>
 </gridlayout>
 
 <!-- <svelte:options immutable={true}/> -->
